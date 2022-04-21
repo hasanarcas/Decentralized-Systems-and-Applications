f87801b22209d1ace8a2a198e185c3ce49b6de82
hasanarcas <hasanarcas@gmail.com>
2022-04-21 10:49:21 +0300

    Created Repo

---
+++ DonutVendingMachine/vending-machine-donuts/blockchain/vending.js
@@ -0,0 +1,10 @@
+import Web3 from 'web3';
+
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+
+const vmContract = web3 => {
+    return new web3.eth.Contract(abi, '0x7d6d677924a281BD643DBcc4bfd4bB0Ea05f9074');
+}
+
+
+export default vmContract;
